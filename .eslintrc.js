7bd40599e52ef779d2981fcf911e145d31d38fec
Jonathan Gruber <jonathan.gruber@stud.htwk-leipzig.de>
2018-06-04 09:24:32 +0200

    Disabled prefer default exports rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'airbnb',
   'rules': {
+    'import/prefer-default-export': [0],
     'import/no-extraneous-dependencies': [0],
     'jsx-a11y/anchor-is-valid': [0],
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
