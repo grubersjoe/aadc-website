41dc0b39c451baa59a92442fe1cc712cdcf0d874
Jonathan Gruber <jonathan.gruber@stud.htwk-leipzig.de>
2018-06-07 23:09:30 +0200

    Added social icons to Avatars

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,44 +1,103 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
 import styled from 'styled-components';
 import color from 'color';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/fontawesome-free-solid';
+import { faGithub, faXing } from '@fortawesome/fontawesome-free-brands';
 import { rhythm } from '../utils/typography';
-import { colors } from '../utils/constants';
+import { colors, fontSizes } from '../utils/constants';
 
 const Avatar = (props) => {
   const { size, imgUrl, caption, offsetY } = props;
+  const { website, github, xing } = props.profiles;
 
-  const Figure = styled('figure') `
+  const Figure = styled('figure')`
     width: ${rhythm(size)};
     margin-right: ${rhythm(1.125)};
     margin-bottom: ${rhythm(1)};
     text-align: center;
+    border-radius: .2rem;
+    overflow: hidden;
   `;
 
-  const Image = styled('div') `
+  const Overlay = styled('div')`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 1rem;
+    background: ${color(colors.primary).alpha(0.7).string()};
+    transition: opacity .3s ease-in-out;
+    opacity: 0;
+    font-size: 150%;
+
+
+    &:hover {
+      opacity: ${Object.keys(props.profiles).length ? 1 : 0};
+    }
+
+    a {
+      margin: 0.5rem;
+      color: white;
+
+      &:hover {
+        color: ${color('white').alpha(0.75).string()}
+      }
+    }
+  `;
+
+  const Image = styled('div')`
     width: 100%;
-    height: ${rhythm(size * 5 / 4)};
-    border-radius: .4rem;
+    height: ${rhythm(size * (5 / 4))};
     background-image: url(${imgUrl});
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center ${offsetY};
   `;
 
-
-  const Caption = styled('figcaption') `
-    margin-top: .4rem;
-    line-height: 1.2;
+  const Caption = styled('figcaption')`
+    padding: .4rem .6rem;
+    background: ${color(colors.primary).fade(0.85).string()};
+    font-size: ${fontSizes.small};
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   `;
+
 
   return (
     <Figure {...props}>
-      <a href={imgUrl} target="_blank">
-        <Image />
-      </a>
+      <Image>
+        <Overlay>
+          {
+            website &&
+            <a href={website} title="Website" target="_blank">
+              <FontAwesomeIcon icon={faLink} />
+            </a>
+          }
+          {
+            github &&
+            <a href={github} title="GitHub" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          }
+          {
+            xing &&
+            <a href={xing} title="Xing" target="_blank">
+              <FontAwesomeIcon icon={faXing} />
+            </a>
+          }
+        </Overlay>
+      </Image>
       <Caption>
-        <small dangerouslySetInnerHTML={{ __html: caption }} />
+        <a
+          href={imgUrl}
+          target="_blank"
+          title="Bild in voller Auflösung"
+          dangerouslySetInnerHTML={{ __html: caption }}
+          style={{ color: colors.text }}
+        />
       </Caption>
     </Figure>
   );
@@ -50,11 +109,13 @@ Avatar.propTypes = {
   caption: PropTypes.string.isRequired,
   size: PropTypes.number,
   offsetY: PropTypes.string,
+  profiles: PropTypes.objectOf(PropTypes.string),
 };
 
 Avatar.defaultProps = {
-  size: 5,
+  size: 6,
   offsetY: '10%',
+  profiles: {},
 };
 
 
--- src/pages/team/2017.js
+++ src/pages/team/2017.js
@@ -4,29 +4,38 @@ import Avatar from '../../components/Avatar';
 import { avatarLina, avatarNick, avatarFabi, avatarJo } from '../../images/avatars';
 // import { rhythm } from '../../utils/typography';
 
-const Team = () => {
-  return (
-    <main>
-      <TeamNav />
-      <h1>Team Smart Driving 2017/2018</h1>
-      <p><strong>Teamspecherin: Lina Peters</strong></p>
-      <h2>Kernteam</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        <Avatar size={5} imgUrl={avatarLina} caption="Lina Peters" />
-        <Avatar size={5} imgUrl={avatarNick} caption="Leo Binder" />
-        <Avatar size={5} imgUrl={avatarNick} caption="Nick Fahrendorff" />
-        <Avatar size={5} imgUrl={avatarFabi} caption="Fabian Freihube" />
-        <Avatar size={5} imgUrl={avatarFabi} caption="Philipp Kleinhenz" />
-      </div>
+const Team = () => (
+  <main>
+    <TeamNav />
+    <h1>Team Smart Driving 2017/2018</h1>
+    <p>Teamspecherin: Lina Peters</p>
+    <h2>Kernteam</h2>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <Avatar
+        imgUrl={avatarLina}
+        caption="Lina Peters"
+      />
+      <Avatar imgUrl={avatarNick} caption="Leo Binder" />
+      <Avatar imgUrl={avatarNick} caption="Nick Fahrendorff" />
+      <Avatar imgUrl={avatarFabi} caption="Fabian Freihube" />
+      <Avatar imgUrl={avatarFabi} caption="Philipp Kleinhenz" />
+    </div>
 
-      <h2>Freie Mitarbeiter</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        <Avatar size={5} imgUrl={avatarFabi} caption="Franz Anders" />
-        <Avatar size={5} imgUrl={avatarJo} caption="Jonathan Gruber" />
-        <Avatar size={5} imgUrl={avatarFabi} caption="Fabian ?" />
-      </div>
-    </main>
-  );
-};
+    <h2>Freie Mitarbeiter</h2>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <Avatar imgUrl={avatarFabi} caption="Franz Anders" />
+      <Avatar
+        imgUrl={avatarJo}
+        caption="Jonathan Gruber"
+        profiles={{
+          website: 'https://grubersjoe.de',
+          github: 'https://github.com/grubersjoe',
+          xing: 'https://www.xing.com/profile/Jonathan_Gruber',
+        }}
+      />
+      <Avatar imgUrl={avatarFabi} caption="Fabian ?" />
+    </div>
+  </main>
+);
 
 export default Team;
