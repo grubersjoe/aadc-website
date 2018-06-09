d1f5d71d7911555add64028c02629d9be3e6b500
Jonathan Gruber <jonathan.gruber@stud.htwk-leipzig.de>
2018-06-09 12:53:14 +0200

    Included possible root path prefix in links

--- src/pages/publikationen.js
+++ src/pages/publikationen.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import { getRootPath } from '../utils/helper';
 
-const Publications = () => (
-  <main>
-    <h1>Publikationen</h1>
-    <h2>Software</h2>
-    <ul>
-      <li><a href="/files/src-aadc-2017.zip">Quelltext AADC 2017</a></li>
-      <li><a href="/files/src-aadc-2016.zip">Quelltext AADC 2016</a></li>
-      <li><a href="/files/src-aadc-2015.zip">Quelltext AADC 2015</a></li>
-    </ul>
+const Publications = () => {
+  const rootPath = getRootPath();
 
-    <h2>Wissenschaftliche Arbeiten</h2>
-    <dl>
-      <dt>Fabian Freihube (2016)</dt>
-      <dd>
-        <a href="/files/ba_freihube.pdf">Entwurf und Realisierung autonomer Fahrfunktionen in Modellfahrzeugen</a>
-      </dd>
-      <dt>Georg Jenschmischek (2016)</dt>
-      <dd>
-        <a href="/files/ma_jenschmischek.pdf">Digitale Bildverarbeitung auf einem selbstkonstruierten Modellfahrzeug</a>
-      </dd>
-    </dl>
-  </main>
-);
+  return (
+    <main>
+      <h1>Publikationen</h1>
+      <h2>Software</h2>
+      <ul>
+        <li><a href={`${rootPath}files/src-aadc-2017.zip`}>Quelltext AADC 2017</a></li>
+        <li><a href={`${rootPath}files/src-aadc-2016.zip`}>Quelltext AADC 2016</a></li>
+        <li><a href={`${rootPath}files/src-aadc-2015.zip`}>Quelltext AADC 2015</a></li>
+      </ul>
+
+      <h2>Wissenschaftliche Arbeiten</h2>
+      <dl>
+        <dt>Fabian Freihube (2016)</dt>
+        <dd>
+          <a href={`${rootPath}files/ba_freihube.pdf`}>Entwurf und Realisierung autonomer Fahrfunktionen in Modellfahrzeugen</a>
+        </dd>
+        <dt>Georg Jenschmischek (2016)</dt>
+        <dd>
+          <a href={`${rootPath}files/ma_jenschmischek.pdf`}>Digitale Bildverarbeitung auf einem selbstkonstruierten Modellfahrzeug</a>
+        </dd>
+      </dl>
+    </main>
+  );
+};
 
 export default Publications;
