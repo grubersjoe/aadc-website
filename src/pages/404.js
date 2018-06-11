25701ed0c26829ad8215bea38b215b0e77859ec0
Jonathan Gruber <jonathan.gruber@stud.htwk-leipzig.de>
2018-06-11 09:58:28 +0200

    Simplified 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import LinkButton from '../components/LinkButton';
 
 const NotFoundPage = () => (
   <main>
     <Helmet title="#404" />
     <h1>404</h1>
     <p>Diese Seite existiert nicht.</p>
-    <LinkButton to="/" primary>
-      Neuigkeiten
-    </LinkButton>
   </main>
 );
 
