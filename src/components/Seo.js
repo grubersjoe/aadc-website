b231aa41bbfe12222ad18d74be2ee1f6286c9522
Jonathan Gruber <jonathan.gruber@stud.htwk-leipzig.de>
2018-06-13 14:58:21 +0200

    Added theme color (PWA feature)

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -7,6 +7,7 @@ const Seo = () => (
       name="description"
       content="Website des Studententeams HTWK Smart Driving der Hochschule für Technik, Wirtschaft und Kultur Leipzig. Forschung Entwicklung für das autonome Autofahren."
     />
+    <meta name="theme-color" content="#225e9b" />
     <html lang="de" />
   </Helmet>
 );
