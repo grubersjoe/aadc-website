6ba0243742907e897cd97bbd55e2ba09da5704e9
Jonathan Gruber <jonathan.gruber@stud.htwk-leipzig.de>
2018-07-02 19:44:57 +0200

    Add favicon

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -8,6 +8,8 @@ const Seo = () => (
       content="Website des Studententeams HTWK Smart Driving aus Leipzig – Forschung und Entwicklung für das autonomen Autofahren."
     />
     <meta name="theme-color" content="#225e9b" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <html lang="de" />
   </Helmet>
 );
